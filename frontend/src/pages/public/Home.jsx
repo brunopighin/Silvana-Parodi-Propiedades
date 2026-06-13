ec2c764619f335d5c2bd1d652aa97e0cce2e4d80
Bruno Pighin <brunopighin@hotmail.com>
2026-06-13 16:22:42 -0300

    Quitar indicador de scroll "Deslizá" del hero

--- frontend/src/pages/public/Home.jsx
+++ frontend/src/pages/public/Home.jsx
@@ -137,14 +137,6 @@ export default function Home() {
           </div>
         </div>
 
-
-        {/* Scroll indicator */}
-        <div className="absolute bottom-8 left-1/2 -translate-x-1/2 flex flex-col items-center gap-2 text-white/40 animate-bounce">
-          <span className="text-xs">Deslizá</span>
-          <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" className="w-5 h-5">
-            <path d="M12 5v14M5 12l7 7 7-7"/>
-          </svg>
-        </div>
       </section>
 
       {/* SEARCH */}
